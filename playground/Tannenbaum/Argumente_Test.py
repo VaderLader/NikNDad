45e1bcc3562775d4fd5c4052269419e1e377f573
SohnyBohny <sohny.bean@streber24.de>
2014-12-21 18:50:42 +0100

    Mein erster beabsichtigter Fehler - neugierig? - probiers aus :)

--- playground/Tannenbaum/Argumente_Test.py
+++ playground/Tannenbaum/Argumente_Test.py
@@ -1,9 +1,12 @@
 def ask_ok(prompt, retries=4, complaint='Bitte Ja oder Nein!'):
    while True:
        ok = input(prompt)
-       if ok in ('j', 'J', 'ja', 'Ja'): return True
-       if ok in ('n', 'N', 'ne', 'Ne', 'Nein'): return False
+       if ok in ('j', 'J', 'ja', 'Ja', 'y', '1'): return True
+       if ok in ('n', 'N', 'ne', 'Ne', 'Nein', '0'): return False
        retries = retries - 1
        if retries < 0:
            raise IOError('Benutzer abgelehnt!')
        print(complaint)
+raise IOError('Hier hat sich wohl ein Fehler eingeschlichen :)')
+string = ask_ok('(y/n)', 5, 'Ja')
+print (string)
