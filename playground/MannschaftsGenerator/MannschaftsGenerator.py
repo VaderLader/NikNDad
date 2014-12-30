497951e01065f98233ce56747aba339679e6f632
SohnyBohny <Sohny.Bean@streber24.de>
2014-12-30 18:12:32 +0100

    das Menü wächst - es wurde noch nicht getestet und ist noch nicht komplett fertig

--- playground/MannschaftsGenerator/MannschaftsGenerator.py
+++ playground/MannschaftsGenerator/MannschaftsGenerator.py
@@ -7,25 +7,36 @@ import Player
 
 
 def menue():
+    
     print ('--- Menü ---')
     print ('Drücke die 1 um einen Spieler hinzuzufügen')
     print ('Drücke die 2 um einen Spieler zulöschen')
     print ('Drücke die 3 um dir alle Spieler anzeigenzulassen')
-    menueInput = input('... und bestatige mit Enter\n')
+    menueInput = int(input('... und bestatige mit Enter\n'))
     print (menueInput)
     
     
-   # while menueInput != '1' and menueInput != '2' and menueInput != '3':
-   #     menueInput = input('1, 2 oder 3 - ist das so schwer?\n')
-   #     print (menueInput)
+    while (menueInput != 1) and (menueInput != 2) and (menueInput != 3):
+        menueInput = int(input('1, 2 oder 3 - ist das so schwer?\n'))
+        print (menueInput)
     print ('cool')
     
         
     if menueInput == 1:
-            pass
+        name = input('Wie heißt der neue Spieler?\n')
+        angriff = int(input('Wie gut ist er im Angriff?\n'))
+        abwehr = int(input('Wie gut ist er in der Abwehr?\n'))
+        tor = int(input('Wie gut ist er im Tor?\n'))
+        name = Player.Player(name, angriff, abwehr, tor)
+        name.print()
+        
+        
     elif menueInput == 2:
-            pass
-
+            name = input('Welchen Spieler willst du löschen')
+    elif menueInput == 3:
+        pass
+    menue()
+menue()
 #==============================================================================
 # print("--- MannschaftsGenerator ---")
 # otto = Player.Player("Otto", 10 ,200, 40)
