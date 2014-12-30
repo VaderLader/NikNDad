a8d687c5d829d8be3134043ee21a7a0cea96b15c
SohnyBohny <Sohny.Bean@streber24.de>
2014-12-30 17:32:53 +0100

    Menü erzeugt - noch nicht fertig

--- playground/MannschaftsGenerator/MannschaftsGenerator.py
+++ playground/MannschaftsGenerator/MannschaftsGenerator.py
@@ -5,34 +5,56 @@
 import Team
 import Player
 
-print("--- MannschaftsGenerator ---")
-otto = Player.Player("Otto", 10 ,200, 40)
-popo = Player.Player("Popo", 40, 0, 10)
-otto2 = Player.Player("Otto")
 
-print("Spieler 1:")
-otto.print()
+def menue():
+    print ('--- Menü ---')
+    print ('Drücke die 1 um einen Spieler hinzuzufügen')
+    print ('Drücke die 2 um einen Spieler zulöschen')
+    print ('Drücke die 3 um dir alle Spieler anzeigenzulassen')
+    menueInput = input('... und bestatige mit Enter\n')
+    print (menueInput)
+    
+    
+   # while menueInput != '1' and menueInput != '2' and menueInput != '3':
+   #     menueInput = input('1, 2 oder 3 - ist das so schwer?\n')
+   #     print (menueInput)
+    print ('cool')
+    
+        
+    if menueInput == 1:
+            pass
+    elif menueInput == 2:
+            pass
 
-print("Spieler 2:")
-popo.print()
+#==============================================================================
+# print("--- MannschaftsGenerator ---")
+# otto = Player.Player("Otto", 10 ,200, 40)
+# popo = Player.Player("Popo", 40, 0, 10)
+# otto2 = Player.Player("Otto")
+# 
+# print ("Spieler 1:")
+# otto.print()
+# 
+# print("Spieler 2:")
+# popo.print()
+# 
+# 
+# teamA = Team.Team("A-Team")
+# teamB = Team.Team("B-Team")
+# 
+# teamA.addPlayer(otto)
+# teamA.addPlayer(popo)
+# teamA.addPlayer(popo)
+# teamA.print()
+# #
+# print ("Jetzt fliegt Otto raus!")
+# teamA.removePlayer(otto)
+# #teamA.removePlayer(otto)
+# teamA.print()
+# print(teamA.calcTeampoints())
+# print(teamA.calcTeampoints())
+# print(teamA.calcTeampoints())
+#==============================================================================
 
 
-teamA = Team.Team("A-Team")
-teamB = Team.Team("B-Team")
-
-teamA.addPlayer(otto)
-teamA.addPlayer(popo)
-teamA.addPlayer(popo)
-teamA.print()
-#
-print ("Jetzt fliegt Otto raus!")
-teamA.removePlayer(otto)
-#teamA.removePlayer(otto)
-teamA.print()
-print(teamA.calcTeampoints())
-print(teamA.calcTeampoints())
-print(teamA.calcTeampoints())
-
-
-print("--- Ende ---")
-
+#print("--- Ende ---")
