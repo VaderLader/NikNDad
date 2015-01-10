33774743cd86281b30b77f36e856b378a40772e8
SohnyBohny <Sohny.Bean@streber24.de>
2015-01-10 13:24:48 +0100

    'weiter'('next') button changes from window 1 to 2 window2: scrollArea doesn't work :( I've seached about 2h in the net - but there wasn't a way to fix :(

--- playground/MannschaftsGenerator/Oberfläche.py
+++ playground/MannschaftsGenerator/Oberfläche.py
@@ -5,6 +5,27 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.uic import *
 
+def startWeiter():
+    s1.close()
+    s2.show()
+    
+textBox=[]
+spinAngriff=[]
+spinAbwehr=[]
+spinTor=[]
+
+geoY = 20
+
+
+
+def add():
+    print ('add')
+#==============================================================================
+#      quit = QtGui.QPushButton('Close', self)
+#  quit.setGeometry(10, 10, 60, 35)
+#==============================================================================
+    
+
 app = QApplication(sys.argv)
 s1 = loadUi('Oberfläche.ui')
 s2 = loadUi('Oberfläche2.ui')
@@ -12,6 +33,9 @@ s2 = loadUi('Oberfläche2.ui')
 
 s1.show()
 
-s1.startWeiterBtn.clicked()
-s2.show()
+s1.startWeiterBtn.clicked.connect(startWeiter)
+
+s2.addBtn.clicked.connect(add)
+
+
 sys.exit(app.exec_())
