a49bc64d3a7ed8ddb89cb1922c5ccaf67a66df92
VaderLader <WiederFrieder@gmx.de>
2015-01-20 06:58:04 +0100

    Working, but needs re-design

--- playground/MannschaftsGenerator/Oberfläche.py
+++ playground/MannschaftsGenerator/Oberfläche.py
@@ -31,22 +31,52 @@ def add():
 #==============================================================================
 #      quit = QtGui.QPushButton('Close', self)
 #  quit.setGeometry(10, 10, 60, 35)
-#==============================================================================
+#==============================================================================   
     
 def weiterRechnen():
-    print('Called weiterRechnen in Oberfläche')
+    ''' weiterRechnen() function triggert by clicking "Weiter"-button '''    
+    #print('Called weiterRechnen in Oberfläche')
+    
+    ##################
+    'Read in data with the right data types from the form'
+    'into some lists, which can be used easily afterwards'
+    name=[s2.lineEdit_1.text(), s2.lineEdit_2.text(), s2.lineEdit_3.text(),
+          s2.lineEdit_4.text(), s2.lineEdit_5.text(), s2.lineEdit_6.text(),
+          s2.lineEdit_7.text(), s2.lineEdit_8.text()]
+    angriff=[int(s2.spinBox_1a.text()), int(s2.spinBox_2a.text()),
+             int(s2.spinBox_3a.text()), int(s2.spinBox_4a.text()), 
+             int(s2.spinBox_5a.text()), int(s2.spinBox_6a.text()),
+             int(s2.spinBox_7a.text()), int(s2.spinBox_8a.text())]
+    abwehr=[int(s2.spinBox_1d.text()), int(s2.spinBox_2d.text()),
+            int(s2.spinBox_3d.text()), int(s2.spinBox_4d.text()), 
+            int(s2.spinBox_5d.text()), int(s2.spinBox_6d.text()),
+            int(s2.spinBox_7d.text()), int(s2.spinBox_8d.text())]
+    tor=[int(s2.spinBox_1g.text()), int(s2.spinBox_2g.text()),
+         int(s2.spinBox_3g.text()), int(s2.spinBox_4g.text()), 
+         int(s2.spinBox_5g.text()), int(s2.spinBox_6g.text()),
+         int(s2.spinBox_7g.text()), int(s2.spinBox_8g.text())]
+    check=[s2.checkBox_1.checkState(), s2.checkBox_2.checkState(),
+           s2.checkBox_3.checkState(), s2.checkBox_4.checkState(), 
+           s2.checkBox_5.checkState(), s2.checkBox_6.checkState(),
+           s2.checkBox_7.checkState(), s2.checkBox_8.checkState()]
+    ##################
+    
+    sq = s2.lineEdit_1.text()
+    print('sq=[',sq,']')
+
     MannschaftsGenerator.defMannschaften()
     
     for index, element in enumerate(name):
-        if check[index] == True:
+        print('check[index]=', 'check[',index,']=',check[index])
+        if check[index] == 2:
             print('index=',index)
-            print('name[index]=',name[index])
-            print('angriff[index]=',angriff[index])
+            print('name[',index,']=',name[index])
+            print('angriff[',index,']=',angriff[index])
             
             teamA.addPlayer( Player.Player({'name': name[index],
                                 'attackpoints': angriff[index],
                                 'defencepoints': abwehr[index], 
-                                'keeperpoint': tor[index]}) )
+                                'keeperpoints': tor[index]}) )
     
     MannschaftsGenerator.berechneMannschaften()
     
@@ -69,12 +99,8 @@ s2 = loadUi('Oberfläche2.ui')
 maxErreicht = loadUi('maxAnzahlErreicht.ui')
 showTeams = loadUi('showTeams.ui')
 
+
     
-name=[s2.lineEdit_1, s2.lineEdit_2, s2.lineEdit_3, s2.lineEdit_4, s2.lineEdit_5, s2.lineEdit_6, s2.lineEdit_7, s2.lineEdit_8]
-angriff=[s2.spinBox_1a,s2.spinBox_2a,s2.spinBox_3a,s2.spinBox_4a,s2.spinBox_5a,s2.spinBox_6a,s2.spinBox_7a,s2.spinBox_8a]
-abwehr=[s2.spinBox_1d,s2.spinBox_2d,s2.spinBox_3d,s2.spinBox_4d,s2.spinBox_5d,s2.spinBox_6d,s2.spinBox_7d,s2.spinBox_8d]
-tor=[s2.spinBox_1g,s2.spinBox_2g,s2.spinBox_3g,s2.spinBox_4g,s2.spinBox_5g,s2.spinBox_6g,s2.spinBox_7g,s2.spinBox_8g]
-check=[s2.checkBox_1, s2.checkBox_2, s2.checkBox_3, s2.checkBox_4, s2.checkBox_5, s2.checkBox_6, s2.checkBox_7, s2.checkBox_8]
 
 geoY = 50
 
